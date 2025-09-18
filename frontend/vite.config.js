7016cbe19632c5822a62c4cac9e167006499eccb
taisan10 <tarunbansalsgnr@gmail.com>
2025-09-18 14:51:40 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),
-//     tailwindcss(),
-//   ],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // ✅ Use "@/components/Contact" style imports
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild',
-  },
-});
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
+
+
