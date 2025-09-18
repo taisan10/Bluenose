47b82a2057c0c622ab00d36d7963def1790d6aa7
taisan10 <tarunbansalsgnr@gmail.com>
2025-09-18 14:34:46 +0530

    first commit

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -6,7 +6,7 @@ import OurServices from "../components/OurServices";
 import Process from "../components/Process";
 import WhatWeDo from "../components/WhatWeDo";
 import StatsSection from "../components/Stats";
-import ContactSection from "../components/Contact.jsx";
+import Contact from "../components/Contact.jsx";
 
 function LandingPage() {
   return (
@@ -21,7 +21,7 @@ function LandingPage() {
       <Process />
       <WhatWeDo />
       <StatsSection/>
-      <ContactSection />
+      <Contact />
     </div>
   );
 }
